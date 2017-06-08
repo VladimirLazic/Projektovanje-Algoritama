05c8784f43cf114a89a49e97837f7106196c9396
interstellaracidtrip <ironmanm72@gmail.com>
2017-06-08 20:20:18 +0200

    Fix

--- Zadatak 3/blackjack.py
+++ Zadatak 3/blackjack.py
@@ -4,7 +4,7 @@ import random
 
 SUITS = ['Clubs', 'Spades', 'Hearts', 'Diamonds']
 RANKS = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
-NUMBER_OF_CARDS = 52
+
 
 def cmp(a, b) :
     if(a > b) :
@@ -16,7 +16,7 @@ def cmp(a, b) :
 
 player = 0
 dealer = 0
-options = list()
+
 cards = list()
 
 for i in range(2,12):
@@ -24,23 +24,22 @@ for i in range(2,12):
         cards.append(i)
 
 np.random.shuffle(cards)
+NUMBER_OF_CARDS = len(cards)
 
 def game_play(i):
     global NUMBER_OF_CARDS
-    cards_drawn = 0
+    options = [0]
     if NUMBER_OF_CARDS - 1 < 4:
         print("Not enough cards")
         return 0
     for p in range(2 , NUMBER_OF_CARDS - i - 2):
         player = cards[i] + cards[i+2] + sum(cards[i+4:i+p+2])
-        cards_drawn += 2 + (i+p+2 - i+4)
         if player > 21:
             print("Player is bust")
             options.append(-1 + game_play(i+p+2))
             break
         for d in range(2 , NUMBER_OF_CARDS-i-p):
             dealer = cards[i+1] + cards[i+3] + sum(cards[i+p+2:i+p+d])
-            cards_drawn += 2 + (i+p+d - i+p+2)
             if dealer >= 17:
                 break
         if dealer > 21:
@@ -49,7 +48,7 @@ def game_play(i):
 
         print("End of hand")
         print("Player = ", player, " : Dealer = ", dealer)
-        NUMBER_OF_CARDS -= cards_drawn
+        #NUMBER_OF_CARDS -= cards_drawn
         options.append(cmp(player ,dealer) + game_play(i+p+d))
     return max(options)
 
